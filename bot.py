aedcccb5bda806e28373800dbd8c4046b70b36e5
Gerch17 <tambovtsevy228@mail.ru>
2020-11-15 19:19:14 +0300

    Add feather. Release commit

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
 import os
+import datetime
 
+from datetime import datetime, date, time
 from telebot import types
 
 AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
@@ -8,31 +10,66 @@ MEDIA_PATH = os.environ.get("MEDIA_PATH")
 
 bot = telebot.TeleBot(AUTH_TOKEN)
 
+def dayOfWeek():
+    return datetime.today().weekday()
+
+#Parsing format
+def parseFromat(line):
+    line, ext = os.path.splitext(line)
+    if ext == ".png":
+        return 1
+    elif ext == ".jpg":
+        return 1
+    elif ext == ".PNG":
+        return 1
+    elif ext == ".JPG":
+        return 1
+    elif ext == ".JPEG":
+        return 1
+    else:
+        return 0
+
 @bot.message_handler(commands=['start'])
 def getStarted(message):
     #creating keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("/help")
-    item2 = types.KeyboardButton("...")
+    item2 = types.KeyboardButton("/instruction")
     markup.add(item1, item2)
 
-    bot.send_message(message.chat.id, "Давай начнём! Используй команду /help для большей информации".
+    bot.send_message(message.chat.id, "Давайте начнём! Используй команду /help для большей информации".
     format(message.from_user, bot.get_me()), parse_mode='html',
     reply_markup=markup)
 
+
+@bot.message_handler(commands=['help'])
+def helpDef(message):
+    bot.send_message(message.chat.id, 'Для того, чтобы принять участие в конкурсе, просто отправьте мне Вашу работу как файл. Работы принимаются с четверга по субботу включительно. Можете отправлять несколько работ, но зачтётся только последняя. Если у Вас возникли проблемы с отправкой работы, посмотрите /instruction. У Вашего профиля Telegram должно быть установлено имя пользователя, чтобы мы могли с Вами связаться')
+
+@bot.message_handler(commands=['instruction'])
+def sendinfInstruction(message):
+    bot.send_video(message.chat.id, 'BAACAgIAAxkBAAICKV-xUzwxG0O4g_FwEDhgRaoD2w74AAKbCQACp0qJSbGcszQIsjS_HgQ')
+
+#downloading photos
 @bot.message_handler(content_types=["document"])
 def download(message):
-    chat_id = message.chat.id
-
-    file_infos = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_infos.file_path)
-
-    src = MEDIA_PATH + message.from_user.username + '.jpg'
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    bot.reply_to(message, "Спасибо, фотограифия была добавлена")
+    if message.from_user.username != None:
+        if parseFromat(message.document.file_name) == 1:
+            if (dayOfWeek() == 3 or dayOfWeek() == 4 or dayOfWeek() == 5): 
+                chat_id = message.chat.id
+                file_infos = bot.get_file(message.document.file_id)
+                downloaded_file = bot.download_file(file_infos.file_path)
+                #Here is the path
+                src = "MEDIA_PATH" + message.from_user.username + '.jpg'
+                with open(src, 'wb') as new_file:
+                    new_file.write(downloaded_file)
+                bot.reply_to(message, "Спасибо за участие, фотограифия была добавлена")
+            else:
+                bot.send_message(message.chat.id, "Конкурс закончился. Ждём следующий")
+        else:
+            bot.send_message(message.chat.id, "Поддерживаются форматы только .jpg  и .png")
+    else:
+        bot.send_message(message.chat.id, "Похоже, что у Вас не установлено имя пользователя в Вашем профиле")
 
 bot.polling(none_stop=True)
 
-#docker run -e AUTH_TOKEN=<token> \tg-bot-py-v $(PWD):/chroot
-#scp -r root@165.227.166.237:/root/app/TelegramBotPy/media /home/gerch/Desktop/media
